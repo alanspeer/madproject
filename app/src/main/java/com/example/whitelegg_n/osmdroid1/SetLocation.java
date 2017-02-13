1c755acdcb4a21efaa966eea076055ddad37c873
2speea07 <2speea07@solent.ac.uk>
2017-02-13 16:57:05 +0000

    part of excerices done

---
+++ app/src/main/java/com/example/whitelegg_n/osmdroid1/SetLocation.java
@@ -0,0 +1,19 @@
+package com.example.whitelegg_n.osmdroid1;
+
+/**
+ * Created by 2speea07 on 13/02/2017.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+public class SetLocation extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setl);
+
+    }
+}
